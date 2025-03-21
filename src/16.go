0d8be19d613ea7269903ab2edc84bc8df42caa35
zamaniegotahuro <zamaniegotahuro@gmail.com>
2025-03-21 09:30:21 +0000

    Add 16.go

---
+++ src/16.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define variables
+    x := 5
+    y := 3
+    
+    // Calculate and print result
+    result := x + y
+    fmt.Printf("The sum of %d and %d is: %d\n", x, y, result)
+}
